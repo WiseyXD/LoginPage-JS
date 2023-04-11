e52301d0e7c08914d8d9e37f35cb5b6af99164ce
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2023-04-11 21:49:15 +0530

    LOgin SignUp Page Completed

--- js/index.js
+++ js/index.js
@@ -1,20 +1,40 @@
-const form = documnet.querySelector('.forms');
-const eye = documnet.querySelectorAll('eyehide');
-const links = documnet.querySelector('.links');
-console.log( eye);
-eye.forEach(eyeIcon => {
-  let pwField = eyeIcon.parentElement.querySelectorAll(".password").addEventListener('click', () =>{
-     pwField.forEach((password)=>{
-        if(password.type === "password")
-        {
-            password.type = "text";
+const form = document.querySelector('.form');
+const eye = document.querySelectorAll('.eyehide');
+const links = document.querySelector('.links');
+const showSignUp = document.querySelector('.signup');
+console.log(links);
+eye.forEach(icon => {
+    icon.addEventListener('click', () =>{
+        let pwFields = icon.parentElement.parentElement.querySelectorAll(".toogle");
+        console.log(pwFields);
+        pwFields.forEach(field =>{
+            if(field.type === 'password')
+            {
+                field.type = 'text';
+                console.log("passwrod = text");
+                icon.classList.replace("bx-low-vision","bx-show");
 
-        }
-        else
-        {
-            password.type = "password";
-        }
-     })  
-    
-    })
+            }
+            else
+            {
+                field.type = 'password'
+                console.log("text = text");
+                icon.classList.replace("bx-show","bx-low-vision");
+            }
+        });
+    });
+   
 });
+
+const toggleLink = document.querySelector(".toggleLink");//Sign Up page pe jaane ke liye
+toggleLink.addEventListener("click",(e)=>{
+    e.preventDefault();
+    showSignUp.classList.add("show");
+});
+
+const loginLink = document.querySelector(".loginLink");
+loginLink.addEventListener("click",(e)=>{
+    e.preventDefault();
+    showSignUp.classList.remove("show");
+});
+
